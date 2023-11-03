3b26eff33ada5dd16fedb566d3ce749fe718dff5
A-Gilmore <avrgilmore@gmail.com>
2023-11-03 16:37:55 -0700

    added 2 functions on different pages to visualize and calculate occupancy

--- server.js
+++ server.js
@@ -28,9 +28,96 @@ app.get("/", async (req, res) => {
     // Extract the "timestamp" values and format the response
     const timestamps = data.map((item) => item.timestamp);
 
+
     // Create a plain text response
-    const responseText =
+    let responseText =
       "Timestamps from the last hour:\n" + timestamps.join("\n");
+
+
+    res.send(responseText);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/signins", async (req, res) => {
+  try {
+    const client = new MongoClient(uri, {});
+    await client.connect();
+    const collection = client.db("SlugMeterTest").collection("Times");
+    
+    let responseText = "Today's sign ins sorted by hour:<br><br>"
+
+
+    //set the timeframe for the chart in the gym operational hours
+    const openHour = 6;
+    const closeHour = 23;
+    //get values for a timestamp on the hour
+    let hour = new Date();
+    hour.setMinutes(0);
+    hour.setSeconds(0);
+    hour.setMilliseconds(0);
+    let nextHour = new Date(hour.valueOf());
+    //iterate over the hours querying how many scan-ins in each
+    for(let i = openHour; i < closeHour; i++){
+      hour.setHours(i);
+      nextHour.setHours(i+1);
+      const hourOccupancyData = await collection
+        .countDocuments({ timestamp: { $gte: hour, $lte: nextHour } });
+      responseText += i + ":00 ";
+      //Adds a special html character that allows for 2 consecutive spaces
+      if(i<10){responseText += "&nbsp;";}
+      responseText += " : " + hourOccupancyData + "<br>";
+    }
+
+    res.send(responseText);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/occupancy", async (req, res) => {
+  try {
+    const client = new MongoClient(uri, {});
+    await client.connect();
+    const collection = client.db("SlugMeterTest").collection("Times");
+    
+    
+
+
+    //set the timeframe for the chart in the gym operational hours
+    const openHour = 6;
+    const closeHour = 23;
+    //expected time to stay in gym in hours
+    const expectedDuration = 1.5;
+    //Time interval to visualize. In minutes
+    const checkInterval = 15;
+    let responseText = "Today's estimated occupancy sorted by " + checkInterval + " minute intervals:<br><br>"
+    //get values for a timestamp on the hour
+    let checkTime = new Date();
+    checkTime.setMinutes(0);
+    checkTime.setSeconds(0);
+    checkTime.setMilliseconds(0);
+    checkTime.setHours(openHour);
+    const curTime = new Date();
+    let cutoffTime = new Date();
+
+    //iterate over the intervals querying how many scan-ins in each
+    for(; checkTime < curTime; checkTime.setMinutes(checkTime.getMinutes() + checkInterval)){
+
+      cutoffTime.setHours(checkTime.getHours()-expectedDuration);
+      const hourOccupancyData = await collection
+        .countDocuments({ timestamp: { $gte: cutoffTime, $lte: checkTime } });
+      responseText += checkTime.toTimeString() + " : " + hourOccupancyData + "<br>";
+    }
+
+    cutoffTime.setHours(curTime.getHours()-expectedDuration);
+    const hourOccupancyData = await collection
+        .countDocuments({ timestamp: { $gte: cutoffTime, $lte: curTime } });
+    responseText += curTime.toTimeString() + " : " + hourOccupancyData + "<br>";
+
 
     res.send(responseText);
   } catch (err) {
