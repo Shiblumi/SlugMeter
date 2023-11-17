7f75eb79651713d1ca45c12f0d3e10eeacbb66a8
aigilmor <aigilmor@ucsc.edu>
2023-11-17 11:26:08 -0800

    Updated GraphHours to fetch data from backend. Make sure backend server is running on a different port

--- frontend/slug-meter-react/src/components/graph/GraphHours.js
+++ frontend/slug-meter-react/src/components/graph/GraphHours.js
@@ -1,10 +1,20 @@
 import classes from "./GraphHours.module.css";
+import { useState } from 'react';
 
 function GraphHours(props) {
   /* To-Do: Convert props.text (the day associated with the button) into a mongoDB call
        that gets hourly data for the passed-in day */
 
-  return <div className={classes.graphPositionOutline}>{props.text}</div>;
+       const [text, setText] = useState([]);
+
+       async function fetchData(){
+        const response = await fetch('http://localhost:9000/signins');
+        const responseText = await response.text()
+        setText(responseText);
+      }
+       fetchData();
+
+  return <div className={classes.graphPositionOutline}>{props.text}<br></br>{text}</div>;
 }
 
 export default GraphHours;
