d8d4ff5af3264530601958e909a71dfab3e2ade7
Shiblumi <70780517+Shiblumi@users.noreply.github.com>
2023-11-17 00:35:11 -0800

    Showcasing to Aidan

--- frontend/slug-meter-react/src/components/buttons/ButtonBar.js
+++ frontend/slug-meter-react/src/components/buttons/ButtonBar.js
@@ -2,20 +2,20 @@ import classes from "./ButtonBar.module.css";
 import DayButton from "./DayButton";
 import { useState } from "react";
 
-// MAKE A CHILD THAT REDDEIVES DATA AND RETURNS THE GRAPH
+// MAKE A CHILD THAT REEIVES DATA AND RETURNS THE GRAPH
 function ButtonBar(props) {
   const [day, setDay] = useState(); // set to today
 
   return (
     <div className={classes.bar}>
         {/* Note: Use inline funcs. for onClick; using func. ptrs calls the function on compile, not on click. */}
-      <DayButton text="Sun" onClick={() => setDay("Sun")}/>
-      <DayButton text="Mon" onClick={() => setDay("Mon")}/>
-      <DayButton text="Tue" onClick={() => setDay("Tue")}/>
-      <DayButton text="Wed" onClick={() => setDay("Wed")}/>
-      <DayButton text="Thu" onClick={() => setDay("Thu")}/>
-      <DayButton text="Fri" onClick={() => setDay("Fri")}/>
-      <DayButton text="Sat" onClick={() => setDay("Sat")}/>
+      <DayButton text="Sun" dayState={day} onClick={() => setDay("Sun")}/>
+      <DayButton text="Mon" dayState={day} onClick={() => setDay("Mon")}/>
+      <DayButton text="Tue" dayState={day} onClick={() => setDay("Tue")}/>
+      <DayButton text="Wed" dayState={day} onClick={() => setDay("Wed")}/>
+      <DayButton text="Thu" dayState={day} onClick={() => setDay("Thu")}/>
+      <DayButton text="Fri" dayState={day} onClick={() => setDay("Fri")}/>
+      <DayButton text="Sat" dayState={day} onClick={() => setDay("Sat")}/>
     </div>
   );
 }
--- frontend/slug-meter-react/src/components/buttons/DayButton.js
+++ frontend/slug-meter-react/src/components/buttons/DayButton.js
@@ -1,8 +1,8 @@
 import classes from "./Button.module.css";
 import GraphHours from "../graph/GraphHours";
+import { useEffect } from "react";
 
 function DayButton(props) {
-
   // not needed?
   function getGraph_day(event) {
     return <GraphHours day={props.text}></GraphHours>;
@@ -10,6 +10,7 @@ function DayButton(props) {
 
   function action() {
     props.onClick();
+    console.log(props.dayState);
   }
 
   // function graphShow() {
