feddeebcbf0bea3d1eb4d4c5113c6716fc4986ba
Shiblumi <70780517+Shiblumi@users.noreply.github.com>
2023-11-17 11:32:12 -0800

    Commented out Aidan's WIP code

--- frontend/slug-meter-react/src/components/graph/GraphHours.js
+++ frontend/slug-meter-react/src/components/graph/GraphHours.js
@@ -5,16 +5,17 @@ function GraphHours(props) {
   /* To-Do: Convert props.text (the day associated with the button) into a mongoDB call
        that gets hourly data for the passed-in day */
 
-       const [text, setText] = useState([]);
+      //  const [text, setText] = useState([]);
 
-       async function fetchData(){
-        const response = await fetch('http://localhost:9000/signins');
-        const responseText = await response.text()
-        setText(responseText);
-      }
-       fetchData();
+      //  async function fetchData(){
+      //   const response = await fetch('http://localhost:9000/signins');
+      //   const responseText = await response.text()
+      //   setText(responseText);
+      // }
+      //  fetchData();
 
-  return <div className={classes.graphPositionOutline}>{props.text}<br></br>{text}</div>;
+      // return <div className={classes.graphPositionOutline}>{props.text}<br></br>{text}</div>;
+      return <div className={classes.graphPositionOutline}>{props.text}</div>;
 }
 
 export default GraphHours;
