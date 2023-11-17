530e2e040d8824f7501f5ac3ec4f67485ff8793f
shiblumi <diwilson@ucsc.edu>
2023-11-16 23:27:17 -0800

    Moving code to PC. Includes Masa's suggestion.

--- frontend/slug-meter-react/src/components/buttons/ButtonBar.js
+++ frontend/slug-meter-react/src/components/buttons/ButtonBar.js
@@ -2,23 +2,20 @@ import classes from "./ButtonBar.module.css";
 import DayButton from "./DayButton";
 import { useState } from "react";
 
+// MAKE A CHILD THAT REDDEIVES DATA AND RETURNS THE GRAPH
 function ButtonBar(props) {
-  const [graphIsOpen, setGraphIsOpen] = useState(false);
+  const [day, setDay] = useState(); // set to today
 
-  function setActiveGraph(day) {
-    console.log(day);
-    setGraphIsOpen(true);
-  }
   return (
     <div className={classes.bar}>
         {/* Note: Use inline funcs. for onClick; using func. ptrs calls the function on compile, not on click. */}
-      <DayButton text="Sun" onClick={() => setActiveGraph("Sun")}/>
-      <DayButton text="Mon" onClick={() => setActiveGraph("Mon")}/>
-      <DayButton text="Tue" onClick={() => setActiveGraph("Tue")}/>
-      <DayButton text="Wed" onClick={() => setActiveGraph("Wed")}/>
-      <DayButton text="Thu" onClick={() => setActiveGraph("Thu")}/>
-      <DayButton text="Fri" onClick={() => setActiveGraph("Fri")}/>
-      <DayButton text="Sat" onClick={() => setActiveGraph("Sat")}/>
+      <DayButton text="Sun" onClick={() => setDay("Sun")}/>
+      <DayButton text="Mon" onClick={() => setDay("Mon")}/>
+      <DayButton text="Tue" onClick={() => setDay("Tue")}/>
+      <DayButton text="Wed" onClick={() => setDay("Wed")}/>
+      <DayButton text="Thu" onClick={() => setDay("Thu")}/>
+      <DayButton text="Fri" onClick={() => setDay("Fri")}/>
+      <DayButton text="Sat" onClick={() => setDay("Sat")}/>
     </div>
   );
 }
