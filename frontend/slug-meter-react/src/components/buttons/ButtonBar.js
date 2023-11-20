0627753430bf7b8beda1aee1520d95ea02d7f7f3
Shiblumi <70780517+Shiblumi@users.noreply.github.com>
2023-11-20 15:22:09 -0800

    Moved logic from ButtonBar to its parent, GraphSelection.

--- frontend/slug-meter-react/src/components/buttons/ButtonBar.js
+++ frontend/slug-meter-react/src/components/buttons/ButtonBar.js
@@ -1,44 +1,26 @@
+import GraphSelection from "../graph/GraphSelection";
 import GraphHours from "../graph/GraphHours";
 import classes from "./ButtonBar.module.css";
 import DayButton from "./DayButton";
 import Card from "../ui/Card";
-import { useState } from "react";
 
 // MAKE A CHILD THAT REEIVES DATA AND RETURNS THE GRAPH
 function ButtonBar(props) {
-  const [day, setDay] = useState("Sun"); // Default graph shown (set to today)
 
-  const sample = {
-    Sun: "SUNDAY GRAPH",
-    Mon: "MONDAY GRAPH",
-    Tue: "TUESDAY GRAPH",
-    Wed: "WEDNESDAY GRAPH",
-    Thu: "THURSDAY GRAPH",
-    Fri: "FRIDAY GRAPH",
-    Sat: "SATURDAY GRAPH",
-  };
-
-  function switchDayGraph(day) {
-    setDay(day);
-    // return <GraphHours text={sample[day]} />;
-  }
 
   return (
     <div>
       <div className={classes.bar}>
         {/* Note: Use inline funcs. for onClick; using func. ptrs calls the function on compile, not on click. */}
-        <DayButton text="Sun" dayState={day} onClick={switchDayGraph} />
-        <DayButton text="Mon" dayState={day} onClick={switchDayGraph} />
-        <DayButton text="Tue" dayState={day} onClick={switchDayGraph} />
-        <DayButton text="Wed" dayState={day} onClick={switchDayGraph} />
-        <DayButton text="Thu" dayState={day} onClick={switchDayGraph} />
-        <DayButton text="Fri" dayState={day} onClick={switchDayGraph} />
-        <DayButton text="Sat" dayState={day} onClick={switchDayGraph} />
+        <DayButton text="Sun" onClick={props.onClick} />
+        <DayButton text="Mon" onClick={props.onClick} />
+        <DayButton text="Tue" onClick={props.onClick} />
+        <DayButton text="Wed" onClick={props.onClick} />
+        <DayButton text="Thu" onClick={props.onClick} />
+        <DayButton text="Fri" onClick={props.onClick} />
+        <DayButton text="Sat" onClick={props.onClick} />
       </div>
-      <Card>
-        {day && <GraphHours text={sample[day]} />}
-        
-      </Card>
+
     </div>
   );
 }
--- frontend/slug-meter-react/src/components/graph/GraphSelection.js
+++ frontend/slug-meter-react/src/components/graph/GraphSelection.js
@@ -2,13 +2,29 @@ import classes from "./GraphSelection.module.css";
 import ButtonBar from "../buttons/ButtonBar";
 import GraphHours from "./GraphHours";
 import Card from "../ui/Card";
+import { useState } from "react";
 
 function GraphSelection(props) {
+  const [day, setDay] = useState("Sun"); // Default graph shown (set to today)
+
+  const sample = {
+    Sun: "SUNDAY GRAPH",
+    Mon: "MONDAY GRAPH",
+    Tue: "TUESDAY GRAPH",
+    Wed: "WEDNESDAY GRAPH",
+    Thu: "THURSDAY GRAPH",
+    Fri: "FRIDAY GRAPH",
+    Sat: "SATURDAY GRAPH",
+  };
+
+  function switchDayGraph(day) {
+    setDay(day);
+    // return <GraphHours text={sample[day]} />;
+  }
   return (
     <div className={classes.graphSelectRegion}>
-      <ButtonBar />
-      <Card>
-      </Card>
+      <ButtonBar onClick={switchDayGraph}/>
+      <Card>{day && <GraphHours text={sample[day]} />}</Card>
     </div>
   );
 }
