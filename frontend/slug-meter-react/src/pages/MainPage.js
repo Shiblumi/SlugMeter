71ea6f2954970dd63d9526b7eec157816e81a3d1
aigilmor <aigilmor@ucsc.edu>
2023-11-26 23:56:58 -0800

    adding mainpage to previous commit

--- frontend/slug-meter-react/src/pages/MainPage.js
+++ frontend/slug-meter-react/src/pages/MainPage.js
@@ -1,9 +1,12 @@
 import GraphSelection from "../components/graph/GraphSelection";
+import CurOccupancy from "../components/layout/CurOccupancy";
 import ButtonBar from "../components/buttons/ButtonBar";
 
 function MainPage() {
   return (
     <content>
+      <h2>Current Occupancy</h2>
+      <CurOccupancy />
       <h2>Gym Crowd History</h2>
       <GraphSelection />
     </content>
