bc6a78616293f0a2258b10df67dbb76fcc94131a
Shiblumi <70780517+Shiblumi@users.noreply.github.com>
2023-12-02 16:53:00 -0800

    Turned it into inline condition cuz i'm cool

--- frontend/slug-meter-react/src/components/graph/GraphHours.js
+++ frontend/slug-meter-react/src/components/graph/GraphHours.js
@@ -59,9 +59,7 @@ function UTCtoLabelTime(date) {
   let time = new Date(date);
   let hour = time.getHours();
   if (hour >= 12) {
-    if (hour != 12) {
-      hour -= 12;
-    }
+    hour -= (hour != 12) ? 12 : 0;
     period = "pm";
   }
 
