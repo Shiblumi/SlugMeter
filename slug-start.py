30c669011bb8311ee8ba13173e2b88e8b94c4353
Shiblumi <70780517+Shiblumi@users.noreply.github.com>
2023-12-05 22:44:52 -0800

    Updated launch options for Mac

--- slug-start.py
+++ slug-start.py
@@ -5,9 +5,12 @@ def start_command_in_new_terminal(command):
     if platform.system() == 'Windows':
         # On Windows, use 'start' to open a new command prompt window
         subprocess.Popen(f'start cmd /k "{command}"', shell=True)
-    elif platform.system() in ['Linux', 'Darwin']:
-        # On Linux or macOS, use a terminal emulator (e.g., gnome-terminal, xterm)
+    elif platform.system() == 'Linux':
+        # On Linux, use a terminal emulator (e.g., gnome-terminal, xterm)
         subprocess.Popen([os.getenv('SHELL'), '-c', command])
+    elif platform.system() == 'Darwin':
+        # On macOS, use osascript to open a new terminal window
+        subprocess.Popen(['osascript', '-e', f'tell app "Terminal" to do script "{command}"'])
     else:
         raise Exception("Unsupported operating system")
 
