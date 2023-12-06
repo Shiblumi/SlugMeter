73af8e30536411bf6321c770ad303559d9823318
Jacob Herman <jacobh@image3dstudio.com>
2023-12-05 23:33:24 -0800

    new slug-start

--- slug-start.py
+++ slug-start.py
@@ -3,22 +3,28 @@ import subprocess, os, time, platform
 
 def start_command_in_new_terminal(command):
     if platform.system() == 'Windows':
-        # On Windows, use 'start' to open a new command prompt window
-        subprocess.Popen(f'start cmd /k "{command}"', shell=True)
+        subprocess.Popen(f'start cmd /k "cd /d {os.getcwd()} && {command}"', shell=True)
     elif platform.system() == 'Linux':
-        # On Linux, use a terminal emulator (e.g., gnome-terminal, xterm)
-        subprocess.Popen([os.getenv('SHELL'), '-c', command])
+        subprocess.Popen([os.getenv('SHELL'), '-c', f'cd {os.getcwd()} && {command}'])
     elif platform.system() == 'Darwin':
-        # On macOS, use osascript to open a new terminal window
-        subprocess.Popen(['osascript', '-e', f'tell app "Terminal" to do script "{command}"'])
+        subprocess.Popen(['osascript', '-e', f'tell app "Terminal" to do script "cd {os.getcwd()} && {command}"'])
     else:
         raise Exception("Unsupported operating system")
 
-cmd_start_server = "node backend/Server/server.js"
-cmd_start_frontend = "npm start"
-print("Starting Server.js...")
-start_command_in_new_terminal(cmd_start_server)
-os.chdir("frontend/slug-meter-react")
-time.sleep(3)
-print("Starting React Frontend...")
-start_command_in_new_terminal(cmd_start_frontend)
+# Define paths
+frontend_path = "./frontend/slug-meter-react/"
+backend_path = "./backend/Server/"
+
+# Change to frontend directory and run npm start
+os.chdir(frontend_path)
+print(f"Current directory: {os.getcwd()}")
+start_command_in_new_terminal("npm start")
+
+# Change back to the original directory
+os.chdir("../../")
+print(f"Current directory: {os.getcwd()}")
+
+# Change to backend directory and run node server.js
+os.chdir(backend_path)
+print(f"Current directory: {os.getcwd()}")
+start_command_in_new_terminal("node server.js")
