5488bd9d0193f08bf9576bcd61098cfe254a836b
Shiblumi <70780517+Shiblumi@users.noreply.github.com>
2023-12-05 20:21:12 -0800

    Added layout for dev profiles on /About

--- frontend/slug-meter-react/src/pages/About.js
+++ frontend/slug-meter-react/src/pages/About.js
@@ -1,16 +1,22 @@
 import classes from "./About.module.css";
+import DevProfile from "../components/ui/DevProfile";
+import pic from "../assets/slugpic.png";
+
+
 function PageTwo() {
   return (
     <div>
       <h2 style={{color:'white', marginBottom:'0rem'}}>Created by:</h2>
-      <p style={{color:'white', fontSize:'0.8rem', marginTop:'0rem', marginLeft:'1rem'}}>(Alphabetical order)</p>
+      <p style={{color:'white', fontSize:'0.8rem', marginTop:'0rem', marginLeft:'1.5rem'}}><em>Alphabetical order</em></p>
+      <br />
       <div className={classes.devShowcase}>
-        <div className={classes.devProfile}>a</div>
-        <div className={classes.devProfile}>b</div>
-        <div className={classes.devProfile}>c</div>
-        <div className={classes.devProfile}>d</div>
-        <div className={classes.devProfile}>e</div>
-        <div className={classes.devProfile}>f</div>
+        <DevProfile profilePic={pic} photoAlt="Profile Pic" name="Aidan Gilmore" bio="idk brooo" link="https://github.com/Shiblumi" linkText="Github" />
+        <DevProfile profilePic={pic} photoAlt="Profile Pic" name="Arul Bangari" bio="idk brooo" link="https://github.com/Shiblumi" linkText="Github" />
+        <DevProfile profilePic={pic} photoAlt="Profile Pic" name="Dirk Wilson" bio="idk brooo" link="https://github.com/Shiblumi" linkText="Github" />
+        <DevProfile profilePic={pic} photoAlt="Profile Pic" name="Joshua Angel" bio="idk brooo" link="https://github.com/Shiblumi" linkText="Github" />
+        <DevProfile profilePic={pic} photoAlt="Profile Pic" name="Jacob Herman" bio="idk brooo" link="https://github.com/Shiblumi" linkText="Github" />
+        <DevProfile profilePic={pic} photoAlt="Profile Pic" name="Kaito Kudo" bio="idk brooo" link="https://github.com/Shiblumi" linkText="Github" />
+
       </div>
     </div>
   );
